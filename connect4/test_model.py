d7d5f92c35a278730f062ef5295e55ed6d7a08cb
Prisme <sami.leroux75@gmail.com>
2026-03-26 12:22:54 +0100

    more accurate transforms

--- connect4/test_model.py
+++ connect4/test_model.py
@@ -1,38 +1,25 @@
 import torch
 import os
-from model import Model
-import torchvision.transforms.v2 as T
-from data_loader import Connect4Dataset
-from torch.utils.data import random_split
+from PIL import Image
 from transformer import transform_test
 
-model = torch.load(os.path.join("models", "first_model.pt"), weights_only= False)
+model = torch.load(os.path.join("models", "model4.pt"), weights_only= False, map_location=torch.device('cpu') )
 
+image_name = "current.png"
+image_path = os.path.join("data", "images_sample", image_name)
 
-
-# Create dataset
-dataset = Connect4Dataset(
-    labels_path="data/labels.json",
-    images_dir="data/images",
-    transform=transform_test
-)
-
-# Split into train/validation (80/20)
-train_size = int(0.8 * len(dataset))
-val_size = len(dataset) - train_size
-train_set, val_set = random_split(dataset, [train_size, val_size])
+image = Image.open(image_path).convert("RGB")
+input_image = transform_test(image).unsqueeze(0)
 
 # Get device from model
 device = next(model.parameters()).device
 
 model.eval()
 with torch.no_grad():
-    # Move input to same device as model and add batch dimension
-    input_image = val_set[0][0].unsqueeze(0).to(device)
-    y_true = val_set[0][1].unsqueeze(0).to(device)
+    input_image = input_image.to(device)
     output = model(input_image)
+
 y_pred = torch.argmax(output, dim=1)
-print("y_pred = \n", y_pred)
-print("y_true = \n", y_true.reshape([6,7]).cpu().numpy())
-print(torch.sum((y_pred - y_true), dim=[0,1,2]))
+print(f"Prediction for {image_name}:")
+print(y_pred.reshape(6, 7).cpu().numpy())
     
--- connect4/transformer.py
+++ connect4/transformer.py
@@ -1,11 +1,13 @@
 
 
 import torchvision.transforms.v2 as T
+import torchvision.transforms.v2.functional as F
 
 # Define transforms with color/lighting augmentation
 transform_train = T.Compose([
+    T.RandomAffine(degrees=2, translate=(0.05, 0.05), scale=(0.95, 1.05), shear=8),
     T.Resize((192, 224)),  # height, width
-    T.RandomAffine(degrees=15, translate=(0.15, 0.15), scale=(0.9, 1.1), shear=8),
+    T.Lambda(lambda img: F.crop(img, top=5, left=25, height=160, width=180)),
     T.ColorJitter(brightness=0.8, contrast=0.8, saturation=0.7, hue=0.1),
     T.RandomApply([T.GaussianBlur(kernel_size=3)], p=0.2),
     T.ToTensor(),
@@ -14,6 +16,7 @@ transform_train = T.Compose([
 
 transform_test = T.Compose([
     T.Resize((192, 224)),  # height, width
+    T.Lambda(lambda img: F.crop(img, top=5, left=25, height=160, width=180)),
     T.ToTensor(),
     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
